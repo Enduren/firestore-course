ebece47ca23e7f63a342308d0e3b41bc0d6dfe7d
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2025-05-31 20:34:31 -0500

    add Button.jsx,index,Todo,and firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,102 @@
 import "./App.css";
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-
-//doc, getDoc, getFirestore
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
+import { auth } from "./lib/firebase";
+import Todo from "./components/Todo";
 
 function App() {
-  const [name, setName] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    async function testFirestore() {
-      const docRef = doc(db, "testCollection", "testDocument");
-
-      const docSnap = await getDoc(docRef);
-
-      //Updates specific fields
-      await updateDoc(docRef, {
-        age: "55",
-        name: "Dwayne",
-      });
-
-      if (docSnap.exists()) {
-        setName(docSnap.data().name);
+    // Check the user's authentication state when the app loads
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        // User is signed in
+        setUser(currentUser);
       } else {
-        console.log("Document doesnt exist");
+        // User is signed out
+        setUser(null);
       }
-    }
-    testFirestore();
-  }, []);
+    });
+
+    return () => unsubscribe();
+  });
+
+  // Sign Up
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        console.log("User signed up:", userCredential.user);
+      })
+      .catch((error) => {
+        console.error("Error signing up:", error);
+      });
+  };
+
+  // Sign In
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        console.log("User signed in:", userCredential.user);
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
+
+  // Sign Out
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+        console.log("User signed out");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
 
   return (
     <>
-      <p>Firestore Check {name}</p>
+      <div>
+        {!user && (
+          <>
+            <h1>Firestore Authentication</h1>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button onClick={signUp}>Sign up</button>
+            <button onClick={signIn}>Sign In</button>
+          </>
+        )}
+      </div>
+
+      {user && (
+        <div>
+          <Todo />
+          <p>Logged in as: {user.email}</p>
+          <button onClick={logOut}>Sign Out</button>
+        </div>
+      )}
     </>
   );
 }
